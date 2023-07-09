7fa378a3880025b39bcaebbc04903202872045f8
Vishal Rana <vr@labstack.com>
2023-07-08 22:01:35 -0700

    Added button for quick start

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
+import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
@@ -24,6 +25,13 @@ function HomepageHeader() {
           src={require('../../static/img/terminal.png').default}
           alt="Terminal"
         />
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/quick-start">
+            Get Started
+          </Link>
+        </div>
       </div>
     </header>
   );
